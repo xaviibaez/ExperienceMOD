0731456a1c4ae120237dfca63c361d6dd06d6c08
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-04-26 12:01:29 +0200

    Initial commit

---
+++ src/main/java/com/xavitoim/experiencemod/ExperienceMod.java
@@ -0,0 +1,31 @@
+package com.xavitoim.experiencemod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("experiencemod")
+public class ExperienceMod
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ExperienceMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
