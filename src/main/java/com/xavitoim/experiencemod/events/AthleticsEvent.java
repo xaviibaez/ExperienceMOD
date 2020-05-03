56a6e29b1410e6b25c2f732b400c75fca020fad9
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-05-03 17:04:22 +0200

    delete run event and refactor jump event into athletics event
    delete run event and refactor jump event into athletics event

--- src/main/java/com/xavitoim/experiencemod/events/AthleticsEvent.java
+++ src/main/java/com/xavitoim/experiencemod/events/AthleticsEvent.java
@@ -6,44 +6,52 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
-import net.minecraft.stats.ServerStatisticsManager;
-import net.minecraft.stats.Stats;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.stats.*;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 
-@Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class RunEvent {
+@Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Bus.FORGE)
+public class AthleticsEvent {
     private static final boolean activated = true;
     private static boolean messageSend;
 
     @SubscribeEvent
-    public static void runEvent(LivingEvent event) {
+    public static void athleticsEvent(LivingJumpEvent event) {
         if(activated) {
             LivingEntity player = event.getEntityLiving();
             World world = player.getEntityWorld();
 
             if(player instanceof PlayerEntity){
                 if(player instanceof ServerPlayerEntity){
-                    Vec3d i = player.getMotion();
                     ServerStatisticsManager statisticsFromPlayer = ((ServerPlayerEntity)player).getStats();
 
+                    int jumpsMade = statisticsFromPlayer.getValue(Stats.CUSTOM.get(Stats.JUMP));
                     int distanceTraveled = statisticsFromPlayer.getValue(Stats.CUSTOM.get(Stats.WALK_ONE_CM)) +
                             statisticsFromPlayer.getValue(Stats.CUSTOM.get(Stats.SPRINT_ONE_CM));
 
-                    if(distanceTraveled >= 3000){
+                    if(jumpsMade >= 300){
                         if(!messageSend){
-                            world.getServer().getPlayerList().sendMessage(new TranslationTextComponent("message.thingtotranslate"));
+                            ((ServerPlayerEntity) player).sendStatusMessage(new TranslationTextComponent("Your jump level is activated"), false);
+                            messageSend = true;
+                        }
+                        player.addPotionEffect(new
+                                EffectInstance(Effects.JUMP_BOOST, Integer.MAX_VALUE, 1));
+                    }
+
+                    if(distanceTraveled >= 5000){
+                        if(!messageSend){
+                            ((ServerPlayerEntity) player).sendStatusMessage(new TranslationTextComponent("Your run level is activated"), false);
                             messageSend = true;
                         }
                         player.addPotionEffect(new
                                 EffectInstance(Effects.SPEED, Integer.MAX_VALUE, 1));
                     }
 
-                    ExperienceMod.LOGGER.info("Test - " + i);
+                    ExperienceMod.LOGGER.info("Jumps Made - " + jumpsMade);
                 }
             }
         }
