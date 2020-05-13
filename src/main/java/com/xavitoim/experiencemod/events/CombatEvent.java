85b50866fb1a7591174b95573c96c0348573ac28
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-05-13 23:51:01 +0200

    Update CombatEvent.java
    reestructure and refactor combat event

--- src/main/java/com/xavitoim/experiencemod/events/CombatEvent.java
+++ src/main/java/com/xavitoim/experiencemod/events/CombatEvent.java
@@ -1,10 +1,6 @@
 package com.xavitoim.experiencemod.events;
 
 import com.xavitoim.experiencemod.ExperienceMod;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.EffectInstance;
@@ -13,39 +9,26 @@ import net.minecraft.stats.ServerStatisticsManager;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
-import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.common.Mod;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
+@Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CombatEvent {
     private static final boolean activated = true;
     private static boolean messageSend;
 
     @SubscribeEvent
     public static void combatEvent(AttackEntityEvent event) {
+        int creaturesKilled = 0;
+
         if(activated) {
             LivingEntity player = event.getPlayer();
 
             if(player instanceof ServerPlayerEntity){
                 ServerStatisticsManager statisticsFromPlayer = ((ServerPlayerEntity) player).getStats();
+                creaturesKilled = statisticsFromPlayer.getValue(Stats.CUSTOM.get(Stats.MOB_KILLS));
 
-                Collection collectionOfEntities =  ForgeRegistries.ENTITIES.getValues();
-                List<Entity> listEntities = new ArrayList<>(collectionOfEntities);
-                int creaturesKilled = 0;
-
-                //TODO probar a ve si va. PONER CHECKS!
-                for(Entity e : listEntities){
-                    if(!e.getType().getClassification().getPeacefulCreature()){
-                        creaturesKilled = creaturesKilled + statisticsFromPlayer.getValue(Stats.ENTITY_KILLED.get(e.getType()));
-                    }
-                }
-
-                //TODO ajustar numeros
-                if(creaturesKilled >= 100){
+                if(creaturesKilled >= 1){
                     if(!messageSend){
                         ((ServerPlayerEntity) player).sendStatusMessage(new TranslationTextComponent("Your mob killed level is activated"), false);
                         ExperienceMod.LOGGER.info("Mobs killed - " + creaturesKilled);
