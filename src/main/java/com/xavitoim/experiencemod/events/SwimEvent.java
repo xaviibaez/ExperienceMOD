f6efb2b507d86a1482322d301f4892eefcb8f9dc
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-05-19 20:25:03 +0200

    Update SwimEvent.java

--- src/main/java/com/xavitoim/experiencemod/events/SwimEvent.java
+++ src/main/java/com/xavitoim/experiencemod/events/SwimEvent.java
@@ -1,29 +1,24 @@
 package com.xavitoim.experiencemod.events;
 
 import com.xavitoim.experiencemod.ExperienceMod;
-import com.xavitoim.experiencemod.utils.helpers.KeyboardHelper;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.stats.ServerStatisticsManager;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-
 public class SwimEvent {
     private static final boolean activated = true;
     private static boolean messageSend;
-    private static boolean potionEffectSend;
+
     @SubscribeEvent
-    public static void athleticsEvent(TickEvent.PlayerTickEvent event) {
+    public static void swimEvent(TickEvent.PlayerTickEvent event) {
         if (activated) {
             LivingEntity player = event.player;
 
