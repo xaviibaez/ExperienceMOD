03cdf6e6b8a3a4689a7757357fadc62ed96192cb
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-05-24 20:18:40 +0200

    Update CombatEvent.java

--- src/main/java/com/xavitoim/experiencemod/events/CombatEvent.java
+++ src/main/java/com/xavitoim/experiencemod/events/CombatEvent.java
@@ -6,9 +6,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
-import net.minecraft.stats.ServerStatisticsManager;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,7 +14,6 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CombatEvent {
     private static final boolean activated = true;
-    private static boolean messageSend;
 
     @SubscribeEvent
     public static void combatEvent(AttackEntityEvent event) {
@@ -26,15 +23,9 @@ public class CombatEvent {
             LivingEntity player = event.getPlayer();
 
             if(player instanceof ServerPlayerEntity){
-                ServerStatisticsManager statisticsFromPlayer = ((ServerPlayerEntity) player).getStats();
-                creaturesKilled = statisticsFromPlayer.getValue(Stats.CUSTOM.get(Stats.MOB_KILLS));
+                creaturesKilled = ((ServerPlayerEntity) player).getStats().getValue(Stats.CUSTOM.get(Stats.MOB_KILLS));
 
                 if(event.getTarget().getType().getClassification() == EntityClassification.MONSTER){
-                    if(!messageSend){
-                        ((ServerPlayerEntity) player).sendStatusMessage(new TranslationTextComponent("Your mob killed level is activated"), false);
-                        ExperienceMod.LOGGER.info("Mobs killed - " + creaturesKilled);
-                        messageSend = true;
-                    }
                     player.addPotionEffect(new
                             EffectInstance(Effects.STRENGTH, 50, creaturesKilled/100000));
                 }
