db54a2ca4c3e6a9c8f165a8e2f0cce89c4996036
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-05-24 20:18:43 +0200

    Update JumpEvent.java

--- src/main/java/com/xavitoim/experiencemod/events/JumpEvent.java
+++ src/main/java/com/xavitoim/experiencemod/events/JumpEvent.java
@@ -7,7 +7,6 @@ import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.stats.*;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,7 +15,6 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Bus.FORGE)
 public class JumpEvent {
     private static final boolean activated = true;
-    private static boolean messageSend;
 
     @SubscribeEvent
     public static void jumpEvent(LivingJumpEvent event) {
@@ -25,15 +23,8 @@ public class JumpEvent {
 
             if(player instanceof PlayerEntity){
                 if(player instanceof ServerPlayerEntity){
-                    ServerStatisticsManager statisticsFromPlayer = ((ServerPlayerEntity)player).getStats();
+                    int jumpsMade = ((ServerPlayerEntity)player).getStats().getValue(Stats.CUSTOM.get(Stats.JUMP));
 
-                    int jumpsMade = statisticsFromPlayer.getValue(Stats.CUSTOM.get(Stats.JUMP));
-
-                    if(!messageSend){
-                        ((ServerPlayerEntity) player).sendStatusMessage(new TranslationTextComponent("Your jump level is activated"), false);
-                        ExperienceMod.LOGGER.info("Jumps Made - " + jumpsMade);
-                        messageSend = true;
-                    }
                     player.addPotionEffect(new
                             EffectInstance(Effects.JUMP_BOOST, 50, jumpsMade/100000));
                 }
