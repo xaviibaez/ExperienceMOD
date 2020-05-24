558e84e7e32f6430143e48b61f5971b15d2a1490
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-05-24 20:19:15 +0200

    Update SwimEvent.java

--- src/main/java/com/xavitoim/experiencemod/events/SwimEvent.java
+++ src/main/java/com/xavitoim/experiencemod/events/SwimEvent.java
@@ -1,13 +1,13 @@
 package com.xavitoim.experiencemod.events;
 
 import com.xavitoim.experiencemod.ExperienceMod;
+import com.xavitoim.experiencemod.utils.helpers.KeyboardHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.stats.ServerStatisticsManager;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,7 +15,6 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class SwimEvent {
     private static final boolean activated = true;
-    private static boolean messageSend;
 
     @SubscribeEvent
     public static void swimEvent(TickEvent.PlayerTickEvent event) {
@@ -29,16 +28,12 @@ public class SwimEvent {
                 int breathUnderWater = statisticsFromPlayer.getValue(Stats.CUSTOM.get(Stats.WALK_UNDER_WATER_ONE_CM));
 
                 if (player.isInWater()) {
-
-                    if (!messageSend) {
-                        ((ServerPlayerEntity) player).sendStatusMessage(new TranslationTextComponent("Your swim level is activated"), false);
-                        ExperienceMod.LOGGER.info("Distance Swam - " + distanceSwam);
-                        messageSend = true;
-                    }
-                    player.addPotionEffect(new
-                            EffectInstance(Effects.DOLPHINS_GRACE, 50, distanceSwam/100000));
                     player.addPotionEffect(new
                             EffectInstance(Effects.WATER_BREATHING, 50, breathUnderWater/100000));
+                    if(KeyboardHelper.isHoldingWASD()){
+                        player.addPotionEffect(new
+                                EffectInstance(Effects.DOLPHINS_GRACE, 50, distanceSwam/100000));
+                    }
                 }
             }
         }
