a6560522a7e283160daae0bceb20c598d949bd7f
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-06-01 21:33:14 +0200

    Update NightEvent.java

--- src/main/java/com/xavitoim/experiencemod/events/NightEvent.java
+++ src/main/java/com/xavitoim/experiencemod/events/NightEvent.java
@@ -1,6 +1,5 @@
 package com.xavitoim.experiencemod.events;
 
-import com.google.common.collect.Iterators;
 import com.xavitoim.experiencemod.ExperienceMod;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
@@ -9,11 +8,8 @@ import net.minecraft.potion.Effects;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.World;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.Iterator;
 
 @Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class NightEvent {
@@ -29,33 +25,14 @@ public class NightEvent {
                 int nightVisionLevel = ((ServerPlayerEntity) player).getStats().getValue(Stats.CUSTOM.get(Stats.SLEEP_IN_BED));
 
                 if (!world.isDaytime()) {
-                    if(!player.getActivePotionEffects().contains(Effects.NIGHT_VISION)){
-                        player.addPotionEffect(new
-                                EffectInstance(Effects.NIGHT_VISION, Integer.MAX_VALUE, nightVisionLevel / 100000));
-                    }
+                    player.addPotionEffect(new
+                            EffectInstance(Effects.NIGHT_VISION, 500, nightVisionLevel / 100000));
                 }
-                else{
-                    //ExperienceMod.LOGGER.info("check 0 - " + player.getActivePotionEffects().size());
-                    player.getActivePotionEffects().remove(Effects.NIGHT_VISION);
-
-
-                    //Iterator<EffectInstance> iterator = ((ServerPlayerEntity) player).activePotionsMap.values().iterator();
-
-                    //TODO hacer esta vaina
-                    /*while(iterator.hasNext()) {
-                        EffectInstance effect = iterator.next();
-                        if(net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(new net.minecraftforge.event.entity.living.PotionEvent.PotionRemoveEvent(player, effect))) continue;
-                        //((ServerPlayerEntity) player).onFinishedPotionEffect(effect);
-                        iterator.remove();
-                    }*/
+                else {
+                    player.addPotionEffect(new
+                            EffectInstance(Effects.NIGHT_VISION, 0, nightVisionLevel / 100000));
                 }
             }
         }
-    }
-
-    @SubscribeEvent
-    public static void nightEvent(PotionEvent.PotionRemoveEvent event) {
-
-
     }
 }
