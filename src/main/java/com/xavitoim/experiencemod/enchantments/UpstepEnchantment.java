6384a4b85481fb1dc337af093db4f9b3bda8a68f
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-06-10 00:18:11 +0200

    Create UpstepEnchantment.java

---
+++ src/main/java/com/xavitoim/experiencemod/enchantments/UpstepEnchantment.java
@@ -0,0 +1,12 @@
+package com.xavitoim.experiencemod.enchantments;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.inventory.EquipmentSlotType;
+
+public class UpstepEnchantment extends Enchantment {
+
+    public UpstepEnchantment(Rarity rarityIn, EnchantmentType typeIn, EquipmentSlotType[] slots) {
+        super(rarityIn, typeIn, slots);
+    }
+}
