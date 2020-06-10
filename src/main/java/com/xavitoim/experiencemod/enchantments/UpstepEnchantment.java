7013e045a8c11306486102f8af7c13d8f1442c0a
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-06-11 00:47:22 +0200

    Update UpstepEnchantment.java

--- src/main/java/com/xavitoim/experiencemod/enchantments/UpstepEnchantment.java
+++ src/main/java/com/xavitoim/experiencemod/enchantments/UpstepEnchantment.java
@@ -1,12 +1,36 @@
 package com.xavitoim.experiencemod.enchantments;
 
+import com.xavitoim.experiencemod.ExperienceMod;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 public class UpstepEnchantment extends Enchantment {
 
     public UpstepEnchantment(Rarity rarityIn, EnchantmentType typeIn, EquipmentSlotType[] slots) {
         super(rarityIn, typeIn, slots);
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public int getMinLevel() {
+        return 1;
+    }
+
+    //TODO check if this works
+    @Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class RunEvent {
+
+        @SubscribeEvent
+        public static void runEvent(TickEvent.PlayerTickEvent event) {
+            com.xavitoim.experiencemod.events.RunEvent.runEventSpeed(event);
+        }
     }
 }
