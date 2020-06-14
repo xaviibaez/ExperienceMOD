d883e848c7038fe878edeeb731cc928859d098e3
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-06-14 22:27:02 +0200

    Update EnchantmentInit.java

--- src/main/java/com/xavitoim/experiencemod/init/EnchantmentInit.java
+++ src/main/java/com/xavitoim/experiencemod/init/EnchantmentInit.java
@@ -1,7 +1,7 @@
 package com.xavitoim.experiencemod.init;
 
 import com.xavitoim.experiencemod.ExperienceMod;
-import com.xavitoim.experiencemod.enchantments.UpstepEnchantment;
+import com.xavitoim.experiencemod.enchantments.RunEnchantment;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
@@ -13,7 +13,7 @@ public class EnchantmentInit {
     public static final DeferredRegister<Enchantment> ENCHANTMENTS = new DeferredRegister<>(
             ForgeRegistries.ENCHANTMENTS, ExperienceMod.MOD_ID);
 
-    public static final RegistryObject<Enchantment> UPSTEP = ENCHANTMENTS.register("upstep",
-            () -> new UpstepEnchantment(Enchantment.Rarity.RARE, EnchantmentType.ARMOR_FEET,
+    public static final RegistryObject<Enchantment> RUN = ENCHANTMENTS.register("run",
+            () -> new RunEnchantment(Enchantment.Rarity.RARE, EnchantmentType.ARMOR_FEET,
                     new EquipmentSlotType[] {EquipmentSlotType.FEET}));
 }
