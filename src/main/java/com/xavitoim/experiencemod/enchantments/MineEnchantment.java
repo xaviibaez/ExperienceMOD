8b36b9df77d023a20bea30fcb69ea74c83dddc53
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-06-16 23:16:13 +0200

    mine enchantment and mine event updated
    mine enchantment and mine event updated

--- src/main/java/com/xavitoim/experiencemod/enchantments/MineEnchantment.java
+++ src/main/java/com/xavitoim/experiencemod/enchantments/MineEnchantment.java
@@ -1,8 +1,18 @@
 package com.xavitoim.experiencemod.enchantments;
 
+import com.xavitoim.experiencemod.ExperienceMod;
+import com.xavitoim.experiencemod.events.MineEventLogic;
+import com.xavitoim.experiencemod.init.EnchantmentInit;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.Map;
 
 public class MineEnchantment extends Enchantment {
     public MineEnchantment(Rarity rarityIn, EnchantmentType typeIn, EquipmentSlotType[] slots) {
@@ -19,5 +29,21 @@ public class MineEnchantment extends Enchantment {
         return 1;
     }
 
+    @Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class MineEvent {
+
+        @SubscribeEvent
+        public static void mineEvent(BlockEvent.BreakEvent event) {
+
+            if(!event.getPlayer().getItemStackFromSlot(EquipmentSlotType.FEET).isEmpty()){
+                ItemStack itemOnFeet = event.getPlayer().getItemStackFromSlot(EquipmentSlotType.FEET);
+                Map<Enchantment, Integer> enchantmentsOnItemOnFeet = EnchantmentHelper.getEnchantments(itemOnFeet);
+
+                if (enchantmentsOnItemOnFeet.containsKey(EnchantmentInit.MINE.get())) {
+                    MineEventLogic.hasteEvent(event);
+                }
+            }
+        }
+    }
 
 }
