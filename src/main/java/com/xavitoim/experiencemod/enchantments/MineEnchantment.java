ff6b441aa7bb9823ee7c59ab9d833ded1b090b61
Xavi Ibáñez Martínez <241xavi@gmail.com>
2020-06-20 22:52:20 +0200

    Update MineEnchantment.java

--- src/main/java/com/xavitoim/experiencemod/enchantments/MineEnchantment.java
+++ src/main/java/com/xavitoim/experiencemod/enchantments/MineEnchantment.java
@@ -1,13 +1,19 @@
 package com.xavitoim.experiencemod.enchantments;
 
 import com.xavitoim.experiencemod.ExperienceMod;
+import com.xavitoim.experiencemod.events.CombatEventLogic;
 import com.xavitoim.experiencemod.events.MineEventLogic;
+import com.xavitoim.experiencemod.init.EnchantmentInit;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import java.util.Map;
 
 
 public class MineEnchantment extends Enchantment {
@@ -28,10 +34,16 @@ public class MineEnchantment extends Enchantment {
     @Mod.EventBusSubscriber(modid = ExperienceMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
     public static class MineEvent {
 
-        //TODO no va, se activa siempre
         @SubscribeEvent
         public static void mineEvent(BlockEvent.BreakEvent event) {
-            MineEventLogic.hasteEvent(event);
+            if(!event.getPlayer().getItemStackFromSlot(EquipmentSlotType.MAINHAND).isEmpty()){
+                ItemStack itemOnHand = event.getPlayer().getItemStackFromSlot(EquipmentSlotType.MAINHAND);
+                Map<Enchantment, Integer> enchantmentsOnItemOnHand = EnchantmentHelper.getEnchantments(itemOnHand);
+
+                if (enchantmentsOnItemOnHand.containsKey(EnchantmentInit.MINE.get())) {
+                    MineEventLogic.hasteEvent(event);
+                }
+            }
         }
     }
 }
